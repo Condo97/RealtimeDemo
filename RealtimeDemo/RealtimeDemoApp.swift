c8a1a34625a177459b8335f5f1312b153d24cadb
Alex C <acoundou@gmail.com>
2024-11-08 09:47:07 -0500

    Initial Commit

---
+++ RealtimeDemo/RealtimeDemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  RealtimeDemoApp.swift
+//  RealtimeDemo
+//
+//  Created by Alex Coundouriotis on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RealtimeDemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ RealtimeDemoTests/RealtimeDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealtimeDemoTests.swift
+//  RealtimeDemoTests
+//
+//  Created by Alex Coundouriotis on 11/8/24.
+//
+
+import Testing
+@testable import RealtimeDemo
+
+struct RealtimeDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
